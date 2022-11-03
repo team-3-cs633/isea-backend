f24aed593fbd92c45db42d96f8821447530586b0
rharry <ryandharry@gmail.com>
2022-11-03 18:14:07 -0400

    run formatter on code

--- src/app.py
+++ src/app.py
@@ -3,7 +3,5 @@ from api import app
 
 if __name__ == "__main__":
     app.run(
-        host="0.0.0.0",
-        port=5555,
-        ssl_context=("./.certs/cert.pem", "./.certs/key.pem")
+        host="0.0.0.0", port=5555, ssl_context=("./.certs/cert.pem", "./.certs/key.pem")
     )
